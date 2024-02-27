8ef99e0c5195016812aba5ea64bc36d5b319554e
Usuario <Usuario@CPD02-M12>
2024-02-27 10:58:55 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppViagem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o valor do combustível: ");
+            double valor = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual será a distância percorrida em km: ");
+            int dist = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qauntos km o veículo faz com um litro de combustível: ");
+            int rend = int.Parse(Console.ReadLine());  
+
+            int consumo = dist / rend;
+
+            double resultado = consumo * valor;
+
+            Console.WriteLine("O valor gasto na viagem será de: " + resultado);
+
+            Console.ReadKey();
+
+        }
+    }
+}
